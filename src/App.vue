43afd3cb8d09409a7346bde473573f69e8664f1b
De Bock Benny <bdebock@protonmail.com>
2025-06-01 14:50:45 +0200

    Add filter by date

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,8 @@ const date = ref(new Date())
 const filteredActivities = computed(() => activities.filter(activity => {
   return activity.from.getFullYear() == date.value.getFullYear() &&
     activity.from.getMonth() == date.value.getMonth()
-}))
+}).sort((a, b) => a.from.getTime() - b.from.getTime())
+)
 
 function UpdateDate(newYear: number, newMonth: number) {
   date.value = new Date(newYear, newMonth)
@@ -29,36 +30,36 @@ function UpdateDate(newYear: number, newMonth: number) {
 const activities: Activity[] = [
   {
     title: "Troelant A / Nova B",
-    from: new Date(2025, 4, 19, 20, 0, 0),
-    till: new Date(2025, 4, 19, 23, 0, 0),
+    from: new Date(2025, 5, 19, 20, 0, 0),
+    till: new Date(2025, 5, 19, 23, 0, 0),
     category: ActivityCategory.APLOEG,
     location: 'Jeugdhuis Troelant, Vleeshouwersstraat 4c, 9112 Sinaai',
   },
   {
     title: "Troelant B / St-Niklase B",
-    from: new Date(2025, 4, 22, 20, 0, 0),
-    till: new Date(2025, 4, 22, 23, 0, 0),
+    from: new Date(2025, 5, 22, 20, 0, 0),
+    till: new Date(2025, 5, 22, 23, 0, 0),
     category: ActivityCategory.BPLOEG,
     location: 'Jeugdhuis Troelant, Vleeshouwersstraat 4c, 9112 Sinaai',
   },
   {
     title: "St-Niklase A / Troelant A",
-    from: new Date(2025, 4, 26, 20, 0, 0),
-    till: new Date(2025, 4, 26, 23, 0, 0),
+    from: new Date(2025, 5, 26, 20, 0, 0),
+    till: new Date(2025, 5, 26, 23, 0, 0),
     category: ActivityCategory.APLOEG,
     location: 'Brandweerkazerne, Nijverheidsstraat 29, 9100 Sint-Niklaas',
   },
   {
     title: "Troelant C / Orix C",
-    from: new Date(2025, 4, 27, 20, 0, 0),
-    till: new Date(2025, 4, 27, 23, 0, 0),
+    from: new Date(2025, 5, 27, 20, 0, 0),
+    till: new Date(2025, 5, 27, 23, 0, 0),
     category: ActivityCategory.CPLOEG,
     location: 'Jeugdhuis Troelant, Vleeshouwersstraat 4c, 9112 Sinaai',
   },
   {
     title: "Zaal niet beschikbaar",
-    from: new Date(2025, 4, 26, 20, 0, 0),
-    till: new Date(2025, 4, 26, 23, 0, 0),
+    from: new Date(2025, 5, 26, 20, 0, 0),
+    till: new Date(2025, 5, 26, 23, 0, 0),
     category: ActivityCategory.ONBESCHIKBAAR,
     location: 'Jeugdhuis Troelant, Vleeshouwersstraat 4c, 9112 Sinaai',
   },
